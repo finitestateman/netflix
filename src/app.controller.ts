cae4bad8fb7032fb944c5eeeaac95d0e306e9c99
Finite Stateman <finitestateman@gmail.com>
2024-12-25 20:22:40 +0900

    06. Get movie id 구현하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 interface Movie {
@@ -27,12 +35,16 @@ export class AppController {
   }
 
   @Get(':id')
-  getMovie() {
-    return {
-      id: 1,
-      name: '해리포터',
-      character: ['해리포터', '엠마왓슨'],
-    };
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      // 이렇게 하면 500 이기 때문에 404로 바꿔준다
+      // throw new Error('존재하지 않는 ID 값의 영화입니다!');
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다!');
+    }
+
+    return movie;
   }
 
   @Post()
