7d0a0e9a39e02099eca744f69d9046a76de29ea5
Finite Stateman <finitestateman@gmail.com>
2024-12-26 01:02:45 +0900

    02. Movie 모듈로 엔드포인트 이전하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
 
 @Module({
@@ -8,10 +6,10 @@ import { MovieModule } from './movie/movie.module';
   imports: [MovieModule],
   // import를 하는 module에서 쓸 수 있게 하고 싶은 것들을 export에 적어준다
   exports: [],
-  controllers: [AppController],
+  controllers: [],
   // service는 로직을 담당하는 provider(=자바의 Bean) 중 하나이다
   // 나중에는 repository나 guard 같은 것들도 providers에 들어간다
   // inject 해줄 수 있는 것들이 provider에 들어간다
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+// export 해줘야 Controller에서 쓸 수 있다
+export interface Movie {
+  id: number;
+  title: string;
+}
 
 @Injectable()
 export class MovieService {
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  private idCounter = 3;
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((m) => m.title.startsWith(title));
   }
 
-  findAll() {
-    return `This action returns all movie`;
+  getMovieById(id: number) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      // 이렇게 하면 500 이기 때문에 404로 바꿔준다
+      // throw new Error('존재하지 않는 ID 값의 영화입니다!');
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다!');
+    }
+
+    return movie;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  createMovie(title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
+    };
+
+    this.movies.push(movie);
+
+    // 다른 건 몰라도 id값은 클라이언트에게 넘겨줘야 한다
+    return movie;
   }
 
-  update(id: number, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+  updateMovie(id: number, title: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} movie`;
+  deleteMovie(id: number) {
+    const movieIndex = this.movies.findIndex((m) => m.id === +id);
+
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+    }
+
+    this.movies.splice(movieIndex, 1);
+
+    // 꼭 id를 반환해줄 필요는 없다(정의하기 나름)
+    return id;
   }
 }
