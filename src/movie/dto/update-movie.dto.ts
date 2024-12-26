744d1d10559a0ba44a7756648d5ed9671597af47
Finite Stateman <finitestateman@gmail.com>
2024-12-26 12:11:12 +0900

    03. Class Validator 프로젝트에 적용하기

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty, IsOptional } from "class-validator";
+
 export class UpdateMovieDto {
+  @IsNotEmpty() // 값이 있을 때 빈 문자열이면 안 된다
+  @IsOptional() // 값이 선택적
   title?: string;
 
+  @IsNotEmpty()
+  @IsOptional()
   genre?: string;
 }
