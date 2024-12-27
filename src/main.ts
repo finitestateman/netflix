b4fd7d8b91333fc060c809f40f96153e8a1f7f7e
Finite Stateman <finitestateman@gmail.com>
2024-12-27 20:44:09 +0900

    06. ValidationPipe 주요 옵션 알아보기

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에 정의되지 않은 프로퍼티를 무시, 기본값: false
+      forbidNonWhitelisted: true, // 무시하지 않고 에러 발생
+      // forbidUnknownValues: true // Validates whether the payload matches the expected DTO type or shape at all
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
