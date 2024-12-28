f6fd4927e2d456a40f76dae167d56ef2d3082b1f
Finite Stateman <finitestateman@gmail.com>
2024-12-28 21:37:10 +0900

    04. TypeORM으로 테이블 생성하기

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,15 +1,33 @@
 import { Exclude, Expose, Transform } from 'class-transformer';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 // @Exclude()
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
   title: string;
 
   // @Expose() // class 단위에 Exclude를 사용한 경우 각 필드에 대해 Expose를 사용할 수 있다
   // @Exclude()
   // @Transform(({ value }) => value.toString().toUpperCase())
+  @Column()
   genre: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
+
+  // soft delete인 경우 필요
+  // @DeleteDateColumn()
+  // deletedAt: Date;
 
   // @Expose() // 메서드에 대해서도 적용할 수 있다
   // get custom() {
