c4a15cce14c7b92d001f7ced619983747b54ca83
Finite Stateman <finitestateman@gmail.com>
2024-12-30 00:29:23 +0900

    10-a. Entity Embedding & Entity Inheritance 실습

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,6 +1,21 @@
 import { Exclude, Expose, Transform } from 'class-transformer';
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
+export class BaseEntity {
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
+
+  // soft delete인 경우 필요
+  // @DeleteDateColumn()
+  // deletedAt: Date;
+}
+
 // @Exclude()
 @Entity()
 export class Movie {
@@ -16,18 +31,16 @@ export class Movie {
   @Column()
   genre: string;
 
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @VersionColumn()
-  version: number;
-
-  // soft delete인 경우 필요
-  // @DeleteDateColumn()
-  // deletedAt: Date;
+  // 객체 임베딩 시 응답도 객체 형식으로 반환된다
+  /**
+    "base": {
+      "createdAt": "2024-12-29T15:26:48.811Z",
+      "updatedAt": "2024-12-29T15:26:48.811Z",
+      "version": 1
+  }
+   */
+  @Column(() => BaseEntity)
+  base: BaseEntity;
 
   // @Expose() // 메서드에 대해서도 적용할 수 있다
   // get custom() {
