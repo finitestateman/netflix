fadbf41acf6d1028287446464ba24d9946ac9d5f
Finite Stateman <finitestateman@gmail.com>
2025-01-28 20:17:02 +0900

    01. Serialization 적용하기

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,19 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
+/**
+ * @Exclude: 직렬화(serialization)에서 제외
+ * { select: false }: DB 쿼리에서 제외
+ * 백엔드에서 해당 필드를 사용하지 않는다면 쿼리할 필요도 없다
+ */
+@Exclude()
 export abstract class BaseTable {
-    @CreateDateColumn()
+    @CreateDateColumn({ select: false })
     public createdAt: Date;
 
-    @UpdateDateColumn()
+    @UpdateDateColumn({ select: false })
     public updatedAt: Date;
 
-    @VersionColumn()
+    @VersionColumn({ select: false })
     public version: number;
 }
