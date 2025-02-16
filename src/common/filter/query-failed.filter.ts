a8c86de6239f83a6530448788362db71bce2d731
Finite Stateman <finitestateman@gmail.com>
2025-02-16 22:48:33 +0900

    Improve: PostgreSQL 에러 코드 처리

--- src/common/filter/query-failed.filter.ts
+++ src/common/filter/query-failed.filter.ts
@@ -2,19 +2,20 @@ import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/commo
 import { QueryFailedError } from 'typeorm';
 import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
 
+const PG_ERROR_UNIQUE_VIOLATION = '23505';
+
 @Catch(QueryFailedError)
-export class QueryFailedFilter implements ExceptionFilter {
-    public catch(exception: QueryFailedError, host: ArgumentsHost): void {
+export class QueryFailedFilter<T extends QueryFailedError> implements ExceptionFilter {
+    public catch(exception: T, host: ArgumentsHost): void {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<ExpressResponse>();
         const request = ctx.getRequest<ExpressRequest>();
 
         const status = HttpStatus.INTERNAL_SERVER_ERROR;
-
         let message = '데이터베이스 에러 발생';
 
-        // TODO: 에러코드로 처리하도록 수정하기(23505)
-        if (exception.message.includes('duplicate key')) {
+        // PostgreSQL 에러 코드 처리
+        if ('code' in exception && exception.code === PG_ERROR_UNIQUE_VIOLATION) {
             message = '중복 키 에러!';
         }
 
